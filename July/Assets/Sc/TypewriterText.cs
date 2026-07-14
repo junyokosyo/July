efdac0b28e79a81e95464f1a32e60ca0c8e70862
junyoko1 <junyoko1@outlook.jp>
2026-07-14 15:57:13 +0900

    [ add ] 会話の情報を受け取り１文字づつ表示するクラスを追加

---
+++ July/Assets/Sc/TypewriterText.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using TMPro;
+/// <summary>
+/// タイプライター風に文字を表示するクラス。1文字ずつ表示する。
+/// </summary>
+public class TypewriterText : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textUI;
+    [SerializeField] private float charInterval = 0.03f;
+    private Coroutine currentRoutine;
+
+    public bool IsTyping { get; private set; }
+
+    /// <summary>1文字表示されるたびに発火。表示された文字を運ぶ。</summary>
+    public event Action<char> OnCharTyped;
+
+    public void ShowLine(string fullText)
+    {
+        if (currentRoutine != null) StopCoroutine(currentRoutine);
+        currentRoutine = StartCoroutine(TypeRoutine(fullText));
+    }
+
+    private IEnumerator TypeRoutine(string fullText)
+    {
+        IsTyping = true;
+        textUI.text = "";
+
+        foreach (char c in fullText)
+        {
+            textUI.text += c;
+            OnCharTyped?.Invoke(c);
+            yield return new WaitForSeconds(charInterval);
+        }
+
+        IsTyping = false;
+    }
+}

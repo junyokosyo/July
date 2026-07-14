54f1776acf25866d5f49fa983b771c610643632e
junyoko1 <junyoko1@outlook.jp>
2026-07-14 15:57:52 +0900

    [ add ] 一文字づつ表示されるごとに音を鳴らすクラスを追加

---
+++ July/Assets/Sc/TypeSoundPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// タイプライター表示中に文字が表示されるたびに効果音を鳴らすクラス。
+/// </summary>
+public class TypeSoundPlayer : MonoBehaviour
+{
+    [SerializeField] private TypewriterText typewriter;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip typeSound;
+
+    private void OnEnable()
+    {
+        typewriter.OnCharTyped += HandleCharTyped;
+    }
+
+    private void OnDisable()
+    {
+        typewriter.OnCharTyped -= HandleCharTyped;
+    }
+
+    private void HandleCharTyped(char c)
+    {
+        if (c == ' ' || c == '\n') return;
+        if (audioSource != null && typeSound != null)
+        {
+            audioSource.PlayOneShot(typeSound);
+        }
+    }
+}
